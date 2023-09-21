2daf0d90e8f6ee74572cede1a16be9f48a442f30
Mehmet ÇAVDAR <mehmet.cavdar@trtworld.com>
2023-09-21 16:10:22 +0300

    patch the Div func to handle divide by zero operation

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package go_math
 
+import "errors"
+
 func Add(a, b int) int {
 
 	return a + b
@@ -17,8 +19,11 @@ func Mul(a, b int) int {
 
 }
 
-func Div(a, b int) int {
+func Div(a, b int) (int, error) {
 
-	return a / b
+	if b == 0 {
+		return 0, errors.New("Divided by zero operation is prohibited")
+	}
+	return a / b, nil
 
 }
