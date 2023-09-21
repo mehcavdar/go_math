8de0a46a27b90e929cb0e2b59b54685da6c541a0
Mehmet ÇAVDAR <mehmet.cavdar@trtworld.com>
2023-09-21 16:18:51 +0300

    add Pow func

--- main.go
+++ main.go
@@ -27,3 +27,21 @@ func Div(a, b int) (int, error) {
 	return a / b, nil
 
 }
+func Pow(a, b int) int {
+
+	if b == 1 {
+		return a
+	}
+	if b == 0 {
+		return 1
+	}
+
+	if b&1 == 0 {
+
+		half := Pow(a, b/2)
+		return half * half
+	}
+
+	return Pow(a, b-1) * a
+
+}
